9d625d930a937027ca546ced16258282f3f7083b
GovindSharma0708 <146430357+GovindSharma0708@users.noreply.github.com>
2024-04-30 22:29:57 +0530

    add

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import { Provider } from 'react-redux';
 import { BrowserRouter as Router } from 'react-router-dom';
 
 import App from './App';
-import store from './store';
+
 
 import { configureStore } from '@reduxjs/toolkit';
 
